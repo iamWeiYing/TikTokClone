235b3ae12e9ae918452b872a8e322b11c5f841b3
iamWeiYing <akashishuusaku@gmail.com>
2024-03-25 10:23:19 +0700

    25/3: complete video

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,5 +1,6 @@
 ﻿import React, { useEffect, useState, useRef } from 'react';
-import { CaretRightOutlined, PauseOutlined } from '@ant-design/icons';
+import { Slider, Tooltip } from 'antd';
+import { CaretRightOutlined, PauseOutlined, SoundFilled, MutedFilled } from '@ant-design/icons';
 import './Video.css';
 
 const secConvert = (sec) => {
@@ -10,40 +11,64 @@ const secConvert = (sec) => {
 
 function Video(props) {
 
-    const [isPlaying, setIsPlaying] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
     const [showControls, setShowControls] = useState(false);
     const videoRef = useRef(null);
 
     const [currentTime, setCurrentTime] = useState([0, 0]);
     const [duration, setDuration] = useState([0, 0]);
-
-    /*useEffect(() => {
-        const { min, sec } = sec2Min(videoRef.current.duration);
-        setDurationSec(videoRef.current.duration);
-        setDuration([min, sec]);
-
-        console.log(videoRef.current.duration);
-        const interval = setInterval(() => {
-            const { min, sec } = sec2Min(videoRef.current.currentTime);
-            setCurrentTimeSec(videoRef.current.currentTime);
-            setCurrentTime([min, sec]);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [isPlaying]);*/
+    const [muted, setMuted] = useState(true);
 
     useEffect(() => {
         const video = videoRef.current;
 
+        const checkViewport = () => {
+            // Kiểm tra nếu video nằm trong viewport
+            const rect = video.getBoundingClientRect();
+            const inViewport = (
+                rect.top >= -50 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 50 &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+
+            if (inViewport) {
+                setIsPlaying(true);
+                videoRef.current.play();
+            } else {
+                setIsPlaying(false);
+                videoRef.current.pause();
+            }
+        };
+
+        const handleScroll = () => {
+            checkViewport();
+        };
+
+        // Đăng ký event listener khi mount component
+        window.addEventListener('scroll', handleScroll);
+
+        // Kiểm tra trạng thái khi component được mount
+        checkViewport();
+
+        // Hủy đăng ký event listener khi unmount component
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const video = videoRef.current;
         const handleTimeUpdate = () => {
             setCurrentTime(video.currentTime || 0);
         };
 
         const handleLoadedMetadata = () => {
-            setCurrentTime(video.currentTime || 0);
+            setCurrentTime(0);
             setDuration(video.duration);
         };
 
-        if (video) {
+        if (videoRef.current) {
             video.addEventListener('timeupdate', handleTimeUpdate);
             video.addEventListener('loadedmetadata', handleLoadedMetadata);
 
@@ -54,12 +79,23 @@ function Video(props) {
         }
     }, []);
 
-    const togglePlayPause = () => {
+    const toggleIsPlaying = () => {
         setIsPlaying(!isPlaying);
+    };
+
+    const togglePlayPause = () => {
+        toggleIsPlaying();
         if (videoRef.current.paused) {
             videoRef.current.play();
         } else {
             videoRef.current.pause();
+        }
+    };
+
+    const toggleMute = () => {
+        if (videoRef.current) {
+            videoRef.current.muted = !videoRef.current.muted;
+            setMuted(videoRef.current.muted);
         }
     };
 
@@ -77,35 +113,62 @@ function Video(props) {
                 ref={videoRef}
                 className="video"
                 onClick={togglePlayPause}
-                loop
-                autoPlay
-                muted
+                onEnded={toggleIsPlaying}
+                muted={muted}
             >
                 <source
                     src={props.videoUrl}
-                    type="video/mp4" 
+                    type="video/mp4"
                 />
                 Your browser does not support the video tag.
             </video>
             {showControls && (
                 <div className="controls">
-                    <button className="play-pause-button" onClick={togglePlayPause}>
-                        {isPlaying ? <PauseOutlined /> : <CaretRightOutlined />}
-                    </button>
-                    <input
-                        type="range"
-                        className="volume-slider"
-                        min="0"
-                        max={duration}
-                        default="0"
-                        step='1'
-                        value={currentTime}
-                        onChange={(e) => {
-                            videoRef.current.currentTime = e.target.value;
-                        }}
-                    />
-                    <div className="duration">
-                        {secConvert(currentTime)} | {secConvert(duration)}
+                    <div className="control-row top">
+                        <button className="play-pause-button" onClick={togglePlayPause}>
+                            {isPlaying ? <PauseOutlined /> : <CaretRightOutlined />}
+                        </button>
+
+                        <Tooltip
+                            title={
+                                <Slider
+                                    vertical
+                                    style={{ height: 70, margin: '12px 0 12px 2px' }}
+                                    tooltip={{ formatter: null }}
+                                    min={0}
+                                    max={1}
+                                    step={0.1}
+                                    value={videoRef.current.volume}
+                                    onChange={(e) => {
+                                        videoRef.current.volume = e
+                                    }}
+                                    disabled={muted}
+                                />
+                            }
+                            placement="top"
+                            arrow={false}
+                        >
+                            <button className="play-pause-button" onClick={toggleMute}>
+                                {muted ? <MutedFilled /> : <SoundFilled />}
+                            </button>
+                        </Tooltip>
+                    </div>
+                    <div className="control-row bottom">
+                        <Slider
+                            style={{ width: '100%', flex: 1 }}
+                            tooltip={{ formatter: null }}
+                            min={0}
+                            max={Number(duration).toFixed(0)}
+                            defaultValue={0}
+                            value={Number(currentTime).toFixed(0)}
+                            onChange={(e) => {
+                                videoRef.current.currentTime = e;
+                            }}
+                        />
+
+                        <div className="duration" style={{ flex: '0 0 auto', marginLeft: 15 }}>
+                            {secConvert(currentTime)} | {secConvert(duration)}
+                        </div>
                     </div>
                 </div>
             )}
--- src/pages/HomePg/HomePg.jsx
+++ src/pages/HomePg/HomePg.jsx
@@ -3,16 +3,23 @@
 import Video from '../../components/video/Video';
 
 function HomePg() {
-    /*const videos = [
+    const videos = [
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
         'https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4',
-    ];*/
+    ];
     return (
-        <Video
-            videoUrl='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
-        />
+        <ul>
+            {videos?.map((video) =>
+                <li key={videos.indexOf(video)} style={{ margin: '400px auto' }}>
+                    <div style={{ width: 505 }}>
+                        <p>Day la video</p>
+                        <Video videoUrl={video} />
+                    </div>
+                </li>
+            )}
+        </ul>
     );
 }
 
